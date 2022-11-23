9d07e7467a87a4e508a50b2ead96f2cb737ce6ec
Leonardo Rodriguez <leonardorm7@hotmail.com>
2022-11-23 08:19:05 -0500

    adding ccs to improve the design

--- Gym_progression/Gym_progression/tasks/forms.py
+++ Gym_progression/Gym_progression/tasks/forms.py
@@ -1,7 +1,12 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Record
 
-class RecordForm(ModelForm):
+class RecordForm(forms.ModelForm):
     class Meta:
         model=Record
         fields =['date','weight','calories_consumed']
+        widgets={
+            'date': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Date'}),
+            'weight': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Record your weight'}),
+            'calories_consumed': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Calories consumed this day'}),
+        }
